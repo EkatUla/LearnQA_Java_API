084a6eeb03428a7f3074685f1e89caccb4652986
EkatUla2022 <reikon@list.ru>
2022-10-13 23:27:28 +0300

    first test

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+
+public class HelloWorldTest {
+
+    @Test
+    public void helloWorld(){
+        String name = "Катя";
+        System.out.println("Привет" + " " + name + "!");
+    }
+}
